84b84116b53493d5800622f5d07d344f8cbb639c
JayB <DooDooRapist@gmail.com>
2019-07-31 21:26:58 -0700

    Changed the routing order,fixed decorator err

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -7,15 +7,17 @@ import { NavComponent } from './nav/nav.component';
 import {ContactComponent} from './contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/atm', pathMatch: 'full', component: AtmComponent },
   {path: '/teller', component: TellerComponent},
   {path: '/contact', component: ContactComponent},
-  {path: '/nav', component: NavComponent}
+  {path: '/nav', component: NavComponent},
+  {path: '**', redirectTo: '/atm', component: AtmComponent},
+  { path: '', redirectTo: '/atm', pathMatch: 'full', component: AtmComponent },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{enableTracing : true})],
   exports: [RouterModule]
 })
-
-export const router: ModuleWithProviders = RouterModule.forRoot(routes);
+//export const router: ModuleWithProviders = RouterModule.forRoot(routes);
+export class router{}
